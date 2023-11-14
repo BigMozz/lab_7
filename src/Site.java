d0b35710a0bf2969af27e34b298acabf46401fe6
moz <armin.mozafari12@gmail.com>
2023-11-14 11:15:37 -0500

    Initial UML without required changes

--- src/Site.java
+++ src/Site.java
@@ -1,4 +1,13 @@
 public class Site
 {
-    System.out.print("hello");
+    public static double TAX_RATE;
+    protected double _units;
+    protected double _rate;
+
+    public Site(double _units, double _rate)
+    {
+        this._units = _units;
+        this._rate = _rate;
+    }
+
 }
